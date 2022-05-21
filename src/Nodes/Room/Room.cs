d11d7a387472f30ea958b3eac010023683f987c7
Aaron Winter <winter.aaron93@gmail.com>
2022-05-21 19:15:32 +0200

    stuff

--- src/Nodes/Room/Room.cs
+++ src/Nodes/Room/Room.cs
@@ -1,4 +1,22 @@
 using Godot;
+using RelEcs;
+using RelEcs.Godot;
+
+public class Tile
+{
+    public int Value;
+    public Tile(int value) => Value = value;
+}
+
+public class Coordinates
+{
+    public Vector2 Value;
+    public Coordinates(int x, int y) => Value = new Vector2(x, y);
+}
+
+public class Blocked { }
+public class Water { }
+public class TileOf { }
 
 public class Room : Node2D
 {
@@ -10,4 +28,42 @@ public class Room : Node2D
     {
         // Position = Coordinates * Size * Vector2.One * 16f;
     }
+
+    // public void _Convert(Marshallable<Commands> commands)
+    // {
+    //     var entity = commands.Value.Spawn();
+    //     entity.Add(this);
+    //     
+    //     var groundMap = GetNode<TileMap>("Map/Ground");
+    //     var waterMap = GetNode<TileMap>("Map/Water");
+    //     var wallMap = GetNode<TileMap>("Map/Walls");
+    //
+    //     for (var y = 0; y < Size.y; y++)
+    //     {
+    //         for (var x = 0; x < Size.x; x++)
+    //         {
+    //             var cell = new Vector2(x, y) * Coordinates;
+    //             
+    //             var tileEntity = commands.Value.Spawn()
+    //                 .Add(new Coordinates(x, y))
+    //                 .Add<TileOf>(entity);
+    //     
+    //             if (waterMap.GetCellv(cell) != TileMap.InvalidCell)
+    //             {
+    //                 tileEntity.Add(new Tile(waterMap.GetCellv(cell)))
+    //                     .Add<Blocked>()
+    //                     .Add<Water>();
+    //             }
+    //             else
+    //             {
+    //                 tileEntity.Add(new Tile(groundMap.GetCellv(cell)));
+    //                 
+    //                 if (wallMap.GetCellv(cell) != TileMap.InvalidCell)
+    //                 {
+    //                     tileEntity.Add<Blocked>();
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
